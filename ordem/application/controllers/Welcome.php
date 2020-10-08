292174966a39c9c8a145847afb31ce2bbfcfc2ab
jpm-services <44644829+jorgevideira@users.noreply.github.com>
2020-10-07 23:02:12 -0300

    first commit

---
+++ ordem/application/controllers/Welcome.php
@@ -0,0 +1,22 @@
+<?php
+defined('BASEPATH') OR exit('No direct script access allowed');
+
+class Welcome extends CI_Controller {
+
+	
+	public function index()
+	{
+		$this->load->view('welcome_message');
+	}
+        
+        
+        public function chamou() {
+        echo 'Voce chamou a função chamou()';
+
+            } 
+            
+            
+    
+}
+
+
